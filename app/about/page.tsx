8f9ad69531be88b60e6fd33eac2b0affbb9570c0
Alex Fernandes <alexfernands@outlook.com>
2025-01-29 00:37:42 -0500

    updates to hero section2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './about.css'; // Make sure to create and style this CSS file
+import Image from 'next/image';
 
 const About: React.FC = () => {
     return (
         <div className="about-container">
             <div className="about-image">
-                <img src="logo.png" alt="Alex Fernandes Picture" />
+                {/* Add width and height properties */}
+                <Image 
+                    src="/logo.png" 
+                    alt="Alex Fernandes Picture" 
+                    width={150} // Adjust the width as needed
+                    height={150} // Adjust the height as needed
+                />
             </div>
             <div className="about-text">
                 <h2>About Me</h2>
